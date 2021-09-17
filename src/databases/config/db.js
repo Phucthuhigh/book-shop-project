2d572ee01f1df5bda56dbfbe246d3d525f6509b9
Phucthuhigh <phucgamingpro@gmail.com>
2021-09-17 19:48:55 +0700

    Deploy 2

--- src/databases/config/db.js
+++ src/databases/config/db.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/book-shop");
+    await mongoose.connect(db, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
     console.log("Connected to Database successfully");
   } catch (error) {
     console.error("Connected to Database failure");
+    process.exit(1);
   }
 }
 
